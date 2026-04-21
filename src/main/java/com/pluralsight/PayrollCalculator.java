1c4bb52a5af218dfe9bfffaf66cb8e36e7821337
josepagan <josepaganrnj@outlook.com>
2026-04-21 16:05:50 -0400

    I cleaned up the code and added notes to better the flow and readability. End of Code

--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -1,49 +1,78 @@
 package com.pluralsight;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class PayrollCalculator {
     public static void main(String[] args) {
-        String file = "src/main/resources/employees.csv";
+
+        String inputFile = "src/main/resources/employees.csv";
+        //Declaring my Reader
         FileReader fileReader;
+        BufferedReader bufferedReader;
         try {
-            fileReader = new FileReader(file);
+            fileReader = new FileReader(inputFile);
         } catch (FileNotFoundException e) {
+            System.err.println("You have ran into a File not found error");
+            throw new RuntimeException(e);
+        }
+       bufferedReader = new BufferedReader(fileReader);
+
+        //Declaring my Writer
+        String fileName = "src/main/resources/payroll.csv";
+        FileWriter myWriter;
+        try {
+            myWriter = new FileWriter(fileName);
+        } catch (IOException e) {
+            System.err.println("You have ran into a Run Time Exception");
             throw new RuntimeException(e);
         }
 
+
         try {
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            //Declaring my Reader
+
             String line = bufferedReader.readLine();
-            line = bufferedReader.readLine();// Skip the first header line
+            line = bufferedReader.readLine(); // Skip the first header line
+
+            //Code will run while there is still a valid value in the following line
             while (line != null) {
 
-                // split up line
+                // Splitting up the line
                 String[] parts = line.split("\\|");
+
+                //Have to .parse from String
                 int id = Integer.parseInt(parts[0]);
                 String name = parts[1];
                 double hoursWorked = Double.parseDouble(parts[2]);
                 double payRate = Double.parseDouble(parts[3]);
 
+                //Using Getters and Setters to declare
                 Employee employee = new Employee();
                 employee.setId(id);
                 employee.setName(name);
                 employee.setHoursWorked(hoursWorked);
                 employee.setPayRate(payRate);
 
-                // get name
-                // create new employee object with correct name
-                // print employee name
-
-                line = bufferedReader.readLine();
+                //|ID|Name|GrossPay
                 System.out.println("Gross pay for " + employee.getId() + " " + employee.getName() + "Is a total of " + employee.calculateGrossPay());
+
+                //Writing in the student file "ID|Name|GrossPay"
+                myWriter.write(employee.getId() + "\\|" + employee.getName() + "\\|" + employee.calculateGrossPay());
+                myWriter.write("\n");
+
+                //This allows for the reader to reset to the following line and back to the top
+                line = bufferedReader.readLine();
+
             }
         } catch (IOException e) {
+            System.err.println("You have ran into a Run Time Exception");
             throw new RuntimeException(e);
         }
-
+        try {
+            myWriter.close();
+        } catch (IOException e) {
+            System.err.println("You have ran into a Run Time Exception");
+            throw new RuntimeException(e);
+        }
     }
 }
