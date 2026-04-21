f14910fde2616bb1912c1e489e1595e15486b920
josepagan <josepaganrnj@outlook.com>
2026-04-21 13:10:57 -0400

    Code works now i was having issue with the first line of the file reading!

--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 public class PayrollCalculator {
     public static void main(String[] args) {
         String file = "src/main/resources/employees.csv";
-        FileReader fileReader ;
+        FileReader fileReader;
         try {
             fileReader = new FileReader(file);
         } catch (FileNotFoundException e) {
@@ -18,29 +18,28 @@ public class PayrollCalculator {
         try {
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             String line = bufferedReader.readLine();
-            String line1 = bufferedReader.readLine();
-            while (line != null){
+            line = bufferedReader.readLine();// Skip the first header line
+            while (line != null) {
 
                 // split up line
-                String [] parts = line.split("\\|");
-                int id  =  Integer.parseInt(parts[0]);
+                String[] parts = line.split("\\|");
+                int id = Integer.parseInt(parts[0]);
                 String name = parts[1];
                 double hoursWorked = Double.parseDouble(parts[2]);
                 double payRate = Double.parseDouble(parts[3]);
-                System.out.println(id);
 
                 Employee employee = new Employee();
                 employee.setId(id);
                 employee.setName(name);
                 employee.setHoursWorked(hoursWorked);
                 employee.setPayRate(payRate);
-                System.out.println(employee.getName());
+
                 // get name
                 // create new employee object with correct name
                 // print employee name
 
-
                 line = bufferedReader.readLine();
+                System.out.println("Gross pay for " + employee.getId() + " " + employee.getName() + "Is a total of " + employee.calculateGrossPay());
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
